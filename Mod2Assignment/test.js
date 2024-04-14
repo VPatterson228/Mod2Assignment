dca8f424a47107f366e4bc1f2e64f78ab37fd381
Viveka Patterson <vivekapatterson@student.olympic.edu>
2024-04-13 23:28:25 -0700

    Mod 2 Skills Demo

--- Mod2Assignment/test.js
+++ Mod2Assignment/test.js
@@ -1,5 +1,19 @@
 /* 3 Examples of basic arithmetic in JS */ 
+console.log(2+2);
+console.log(5*7);
+console.log(8%2);
 
 /* 2 Examples of declaring variables in JS */
-           
+let basketball="Shot";
+console.log(basketball);
+let shot=2;
+console.log(shot);      
 /* 2 Examples of using variables in arithmetic operations in JS */
+let length = 6;
+let width = 7;
+let area = length*width
+console.log(area)
+let x = 12;
+let y =6;
+let sum = x + y
+console.log(sum)
