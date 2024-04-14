4976d0f3ef3cf9e6f9679efa16b3f583f4d77acf
Viveka Patterson <vivekapatterson@student.olympic.edu>
2024-04-13 23:49:52 -0700

    Mod 2 updaye

--- Mod2Assignment/test.js
+++ Mod2Assignment/test.js
@@ -1,19 +1,18 @@
 /* 3 Examples of basic arithmetic in JS */ 
 console.log(2+3);
-console.log(5*9);
-console.log(9%2);
+console.log(5*5);
+console.log(19%8);
 /* 2 Examples of declaring variables in JS */
-let basketball="Shot";
-console.log(basketball)
-const twoPointShot =2;
-console.log(twoPointShot);
-
+let basketball ="Shot";
+console.log(basketball);
+let points = 2;
+console.log(points)
 /* 2 Examples of using variables in arithmetic operations in JS */
-let length =5;
-let width =5;
-let area= length*width;
-console.log(area)
-let x = 10;
-let y =15;
-let sum = x+y;
-console.log(sum)
+let length = 5;
+let width  =3;
+let area =length*width;
+console.log(area);
+let x=12;
+let y =5;
+let difference = x-y;
+console.log(difference)
